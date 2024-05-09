97390fce15cb324d988a72d2c609a900037cb933
Anthony Phillips <anthony.s.phillips888@gmail.com>
2024-05-09 10:53:37 -0400

    Create callback fucntion passed into find

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+//general function
+function superbowlWin(record) {
+  const gameResult = record.find(callback)
+  
+
+  
+}
+ 
+function callback(record) {
+  return record.result === "W"
+}
+//creating callback function that result is w and result the year
+
+
+
+
